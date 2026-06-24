c467253e2231e65297718b4162b2b5196b7a9b24
Haolader91 <haolader910@gmail.com>
2026-06-24 12:09:39 +0600

    Add user dashboard pages and booking functionalities

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
     ],
   },
